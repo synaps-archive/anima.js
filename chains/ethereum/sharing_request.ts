0d5f0df2dae3f781769b224dd0d876e1253a5b83
Lucas ALLOIN <lucas.alloin915@gmail.com>
2022-04-06 17:45:24 +0200

    fix: modifie sharing request eip712 payload to match new specs

--- chains/ethereum/sharing_request.ts
+++ chains/ethereum/sharing_request.ts
@@ -20,34 +20,26 @@ export function SharingRequest(
     },
     message: {
       ...message,
-      request: {
-        ...message.request,
+      authorization: {
+        ...message.authorization,
       },
     },
     primaryType: "Main",
     types: {
       Main: [
-        { name: "request", type: "Request" },
+        { name: "authorization", type: "Authorization" },
         { name: "verifier", type: "Verifier" },
         { name: "owner", type: "Owner" },
       ],
-      Request: [
-        {
-          name: "resource",
-          type: "string",
-        },
-        {
-          name: "shared_at",
-          type: "string",
-        },
-        {
-          name: "credential",
-          type: "string",
-        },
-        {
-          name: "attributes",
-          type: "Attributes",
-        },
+      Authorization: [
+        { name: "specs", type: "string" },
+        { name: "shared_at", type: "uint64" },
+        { name: "source", type: "Source" },
+        { name: "attributes", type: "Attributes" },
+      ],
+      Source: [
+        { name: "id", type: "string" },
+        { name: "specs", type: "string" }
       ],
       Owner: [
         { name: "id", type: "string" },
